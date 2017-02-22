8e614a6f568f43d28bfdabc95e4d88a4fc61e3a6
harsha <reddy.harsh92@gmail.com>
2017-02-22 11:15:12 +0530

    added microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222025156) do
+ActiveRecord::Schema.define(version: 20170222052642) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
